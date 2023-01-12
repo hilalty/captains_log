75c74dd2cdbf9b275f2108dea39c6f49b6ad00c6
Hilal Yilmaz <hilaltiftik@gmail.com>
2023-01-12 02:00:20 -0800

    mongoose installed and configured

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+  
+  mongoose.connection.once("open", () => {
+    console.log("connected to mongo");
+  });
 
 //new route
 app.get("/logs/new", (req, res) => {
